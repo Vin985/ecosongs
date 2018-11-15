63757764bbdcff296586938abf1546d696ef4680
Vin985 <sylvain.christin@gmail.com>
2018-11-15 00:27:00 -0500

    plots plots plots

--- src/plots/aci_plot.py
+++ src/plots/aci_plot.py
@@ -1,21 +1,46 @@
 
 import datetime
 
+import numpy as np
+import pandas as pd
+
 import feather
 from plotnine import (aes, facet_grid, geom_line, geom_smooth, ggplot,
                       scale_x_continuous)
 
 sites = ["Igloolik", "East Bay", "Barrow", "Burntpoint Creek", "Canning River", "Svalbard"]
 
+site_data = pd.read_excel("/home/vin/Doctorat/data/datasheet/2018/sites_deployment_2018.xlsx")
+
+site_data
+
 # aci = feather.read_dataframe("src/plots/ACI.feather")
 aci = feather.read_dataframe("ACI.feather")
+aci = aci.loc[aci.site.isin(sites)]
 aci["julian"] = aci["date"].dt.dayofyear
 aci["hour"] = aci["date"].dt.hour
 aci = aci.sort_values(["site", "plot", "julian", "date"])
 aci = aci.loc[(aci["julian"] > 155) & (aci["julian"] < 220)]
 aci = aci.loc[(aci["ACI"] < 50000)]
 res = aci.loc[aci["denoised"] == False]
-res = res.groupby(["site", "julian"]).mean()
+
+
+def check_dates(df, site_data):
+    plot = site_data.loc[site_data["plot"] == df.name]
+    res = df
+    if not plot.empty:
+        start = plot.depl_start.iloc[0]
+        end = plot.depl_end.iloc[0]
+        if not pd.isnull(start):
+            res = df.loc[(df["date"] > start) & (df["date"] < end)]
+    res.ACI = (res.ACI - res.ACI.mean()) / res.ACI.std()
+    return res
+
+
+res = res.groupby(["plot"]).apply(check_dates, site_data)
+res
+
+res = res.groupby(["site", "julian"]).ACI.mean()
 res = res.reset_index()
 res
 # print(aci.loc[aci["site"] == "Igloolik"])
@@ -30,9 +55,9 @@ def label_x(dates):
 
 (ggplot(data=res, mapping=aes(x='julian', y='ACI', colour='site'))
     + facet_grid("site~", scales="free")
-    # + geom_line()
-    + geom_smooth(method="mavg", method_args={"window": 5})
-    + scale_x_continuous(labels=label_x)).save("figs/ACI_all_noise_mavg.png", height=10, width=8, dpi=150)
+    + geom_line()
+    + geom_smooth(method="mavg", se=False, method_args={"window": 4, "center": True, "min_periods": 1})
+    + scale_x_continuous(labels=label_x))  # .save("figs/ACI_all_noise_mavg.png", height=10, width=8, dpi=150)
 
 # Test sans bruit
 aci2 = aci.loc[aci["site"] == "East Bay"]
@@ -43,5 +68,5 @@ res2 = res2.reset_index()
 (ggplot(data=res2, mapping=aes(x='julian', y='ACI', colour='denoised'))
     + facet_grid("denoised~", scales="free")
     # + geom_line()
-    + geom_smooth(method="mavg", method_args={"window": 5})
+    + geom_smooth(method="mavg", se=False, method_args={"window": 3, "center": True, "min_periods": 1})
     + scale_x_continuous(labels=label_x))  # .save("figs/ACI_EABA_mavg5.png", height=10, width=8, dpi=150)
