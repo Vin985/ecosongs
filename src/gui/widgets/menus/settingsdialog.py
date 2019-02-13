c2163d41d71c1ee717d9a4c764a8ce65ac3a4e87
Vin985 <sylvain.christin@gmail.com>
2019-02-13 16:14:34 -0500

    code style

--- src/gui/widgets/menus/settingsdialog.py
+++ src/gui/widgets/menus/settingsdialog.py
@@ -1,14 +1,14 @@
-from gui.utils.settings import Settings
+from PySide2.QtWidgets import QDialog
+
 from gui.widgets.menus.ui.settingsdialog_ui import Ui_SettingsDialog
-from PySide2.QtWidgets import QDialog, QWidget
 
 
 class SettingsDialog(QDialog, Ui_SettingsDialog):
     def __init__(self):
-        super(self.__class__, self).__init__()
+        super().__init__()
         self.setupUi(self)
-        self.linkEvents()
+        self.link_events()
 
-    def linkEvents(self):
+    def link_events(self):
         # Navigation: change page when icon is clicked
         self.menu_categories.currentRowChanged.connect(self.menu_pages.setCurrentIndex)
