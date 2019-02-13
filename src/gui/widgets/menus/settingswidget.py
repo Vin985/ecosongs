bc492cab5b7b5ef18d8ab96cfd7feb6133973b62
Vin985 <sylvain.christin@gmail.com>
2019-02-13 16:12:23 -0500

    refactor: renamed settingspage to settingswidget

---
+++ src/gui/widgets/menus/settingswidget.py
@@ -0,0 +1,23 @@
+from PySide2.QtWidgets import QWidget
+
+from gui.utils.settings import Settings
+
+
+class SettingsWidget(QWidget):
+
+    def __init__(self, parent=None, local=False):
+        super().__init__(parent)
+        self.local = local
+        self.group = ""
+        self.settings = {}
+
+    def save_setting(self, value, key, context=""):
+        if context:
+            context = "/" + context
+        settings = Settings()
+        settings.setValue(self.group + context + "/" + key, value)
+
+    def update_setting(self, value, key):
+        self.settings[key] = value
+        if not self.local:
+            self.save_setting(key, value, self.CONTEXT)
