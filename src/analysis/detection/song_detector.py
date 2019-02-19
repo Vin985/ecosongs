803ff75e723ebbd505cc93ad6e32e3268038d9c4
Vin985 <sylvain.christin@gmail.com>
2019-02-18 19:44:25 -0500

    add events to database

--- src/analysis/detection/song_detector.py
+++ src/analysis/detection/song_detector.py
@@ -14,6 +14,21 @@ class SongEventsTable(TableModel):
     def __init__(self, df=None, dbmanager=None):
         TableModel.__init__(self, self.COLUMNS, df=df, dbmanager=dbmanager)
 
+    def add(self, new, save=False, replace=True):
+        # TODO: check duplicates
+        # TODO: add idx column?
+        # TODO: check duplicates
+        print(self.df)
+        new = self.check_ids(new)
+        if replace:
+            to_remove = new["recording_id"].unique()
+            self.df = self.df.loc[~self.df.recording_id.isin(to_remove)]
+        self.df = self.df.append(new, ignore_index=True, sort=True)
+        self.save(update=True)
+
+    def get_events(self, recording_id):
+        return self.df[self.df["recording_id"] == recording_id]
+
 
 class SongsSummaryTable(TableModel):
     TABLE_NAME = "songs_summary"
