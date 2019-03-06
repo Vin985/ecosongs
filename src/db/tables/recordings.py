2255df9c805bfb5a3cac651b111e2f6ff5234c51
Vin985 <sylvain.christin@gmail.com>
2019-03-06 15:09:33 -0400

    set id as index added get_recoding by id

--- src/db/tables/recordings.py
+++ src/db/tables/recordings.py
@@ -27,8 +27,16 @@ class RecordingsTable(TableModel):
         """
         to_load = [idx for idx in indexes if idx not in self.recordings]
         if to_load:
-            to_load = self._df.iloc[indexes]
-            to_update = {row.Index: Recording(row._asdict(), spec_opts=spec_opts)
+            to_load = self._df.loc[indexes]
+            to_update = {row.id: Recording(row._asdict(), spec_opts=spec_opts)
                          for row in to_load.itertuples(index=True)}
             self.recordings.update(to_update)
         return [self.recordings[idx] for idx in indexes]
+
+    def get_recording(self, idx, spec_opts=None):
+        if idx in self.recordings.keys():
+            rec = self.recordings[idx]
+        else:
+            rec = Recording(self._df.loc[idx].to_dict(), spec_opts=spec_opts)
+            self.recordings[idx] = rec
+        return rec
