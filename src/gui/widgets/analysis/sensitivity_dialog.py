95168d43a516ce563c8a1be5b16fa04061772d2f
Vin985 <sylvain.christin@gmail.com>
2020-04-01 17:11:23 -0300

    sensitivity analysis process

--- src/gui/widgets/analysis/sensitivity_dialog.py
+++ src/gui/widgets/analysis/sensitivity_dialog.py
@@ -19,12 +19,18 @@ from gui.widgets.analysis.sensitivity_worker import SensitivityWorker
 class SensitivityDialog(ProgressDialog):
 
     launch_analysis = Signal()
+    save_results = Signal(str, str)
 
-    def __init__(self, recordings, tags, predictions, parent=None):
+    def __init__(self, predictions, recordings, tags, audio_path, labels_path, parent=None):
         super().__init__(parent)
-        self.worker = SensitivityWorker(recordings, tags, predictions)
+        self.worker = SensitivityWorker(
+            predictions=predictions, recordings=recordings, tags=tags)
         self.options = SensitivityOptions(self)
         self.content_layout.addWidget(self.options)
+        self.save = False
+        self.overwrite = False
+        self.audio_path = audio_path
+        self.labels_path = labels_path
         self.link_events()
         # self.adjustSize()
 
@@ -32,14 +38,52 @@ class SensitivityDialog(ProgressDialog):
         super().link_events()
         self.launch_analysis.connect(
             self.worker.perform_analysis, type=Qt.QueuedConnection)
+        self.save_results.connect(
+            self.worker.save_results, type=Qt.QueuedConnection)
+        self.worker.saved.connect(self.results_saved)
 
     @Slot()
     def start(self):
         print("clicking start")
-        self.worker.options = self.options.get_options()
+        opts = self.options.get_options()
+        self.worker.options = opts
+        self.save = opts["save"]
+        self.overwrite = opts["overwrite"]
         self.launch_analysis.emit()
         self.started = time.time()
 
     @Slot()
     def process_results(self):
-        pass
+        params = len(self.worker.parameters)
+        self.duration = (time.time() - self.started)
+        log_text = ("Calculated stats for %d combinations in %0.3f seconds (%0.3fs/combination)" %
+                    (params, self.duration, self.duration / params))
+        if self.save:
+            log_text += ". Saving results, please wait..."
+            self.save_results.emit(self.audio_path, self.labels_path)
+        self.log(log_text)
+
+    def results_saved(self):
+        super().process_results()
+
+    def save_results2(self):
+        results = self.worker.results
+        analysis_options_table = qApp.tables.analysis_options
+        events_table = qApp.tables.song_events
+        stats_table = qApp.tables.detector_statistics
+        for params, stats, events in results:
+            print(" saving params " + str(params))
+            params["audio_path"] = self.audio_path
+            params["labels_path"] = self.labels_path
+            options_id = analysis_options_table.add(
+                params, analysis_type="activity_prediction", save=False, replace=False)
+
+            # events["analysis_options"] = options_id
+            # events_table.add(events, save=False, replace=True)
+
+            stats_df = pd.DataFrame([stats])
+            stats_df["analysis_options"] = options_id
+            stats_table.add(stats_df, save=False)
+        analysis_options_table.save()
+        events_table.save()
+        stats_table.save()
--- src/gui/widgets/analysis/sensitivity_worker.py
+++ src/gui/widgets/analysis/sensitivity_worker.py
@@ -1,29 +1,84 @@
 
-from PySide2.QtCore import Slot
+import traceback
+from itertools import product
 
+import pandas as pd
+from PySide2.QtCore import Slot, qApp, Signal
+
+from analysis.detection.detector_evaluation import (mp_get_stats,
+                                                    mp_initialize_evaluator)
 from gui.threads.thread_worker import ThreadWorker
+from utils.commons import range_list
 
 
 class SensitivityWorker(ThreadWorker):
-    def __init__(self, recordings, tags, predictions):
+
+    saved = Signal()
+
+    def __init__(self, predictions, recordings, tags):
         super().__init__()
         self.recordings = recordings
         self.tags = tags
         self.predictions = predictions
+        self.parameters = []
 
-        # self.perform_analysis(indexes.mp_compute_index_chunk,
-        #                       initializer=indexes.mp_initialize_index,
-        #                       initargs=(index_type, self.options["initargs"]))
-
-    # @Slot()
-    # def detect_songs(self):
-    #     self.perform_analysis(song_detector.mp_detect_songs_chunk,
-    #                           initializer=song_detector.mp_initialize_detector,
-    #                           initargs=self.options["initargs"])
     @Slot()
     def perform_analysis(self):
+        self.results = []
+        self.computing.emit()
         print("launching sensitivity analysis")
+        opts = self.options["analysis_options"]
+        # opts["min_activity_start"] = 0.8
+        # opts["min_activity_end"] = 0.9
+        # opts["end_threshold_start"] = 0.5
+        # opts["end_threshold_end"] = 0.6
+        # opts["min_duration_start"] = 0.1
+        # opts["min_duration_end"] = 0.12
+
+        try:
+            min_activities = range_list(
+                opts["min_activity_start"], opts["min_activity_end"], opts["min_activity_step"])
+            end_thresholds = range_list(
+                opts["end_threshold_start"], opts["end_threshold_end"], opts["end_threshold_step"])
+            min_durations = range_list(
+                opts["min_duration_start"], opts["min_duration_end"], opts["min_duration_step"])
+        except Exception:
+            self.error.emit(traceback.format_exc())
+
+        self.parameters = list(
+            product(*[min_activities, end_thresholds, min_durations]))
+
+        print(self.parameters)
+        self.options["chunksize_percent"] = 5
         print(self.options)
+        self.map(self.parameters, mp_get_stats,
+                 initializer=mp_initialize_evaluator,
+                 initargs=(self.predictions, self.recordings, self.tags))
+        self.done.emit()
+
+    def save_results(self, audio_path, labels_path):
+        try:
+            analysis_options_table = qApp.tables.analysis_options
+            events_table = qApp.tables.song_events
+            stats_table = qApp.tables.detector_statistics
+            for params, stats, events in self.results:
+                params["audio_path"] = audio_path
+                params["labels_path"] = labels_path
+                options_id = analysis_options_table.add(
+                    params, analysis_type="activity_prediction", save=False, replace=False)
+
+                events["analysis_options"] = options_id
+                events_table.add(events, save=False, replace=True)
+
+                stats_df = pd.DataFrame([stats])
+                stats_df["analysis_options"] = options_id
+                stats_table.add(stats_df, save=False)
+            analysis_options_table.save()
+            events_table.save()
+            stats_table.save()
+        except Exception:
+            self.error.emit(traceback.format_exc())
+        self.saved.emit()
 
         # self.results = []
         # self.computing.emit()
