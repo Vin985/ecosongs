db829a391cb37c705913eba99fbd30fe7f8d07db
Vin985 <sylvain.christin@gmail.com>
2020-04-17 15:12:50 -0300

    improved options saving and loading: convert list to columns make sure data is correctly formatted for saving

--- src/db/tables/analysis_options.py
+++ src/db/tables/analysis_options.py
@@ -13,39 +13,72 @@ class AnalysisOptionsTable(TableModel):
         TableModel.__init__(self, self.COLUMNS, df=df, dbmanager=dbmanager)
 
     def add(self, options, analysis_type, save=False, replace=True):
-        opts = self.format_options(options)
-        existing = None
-        opt_id = 0
 
-        if opts and self.df.shape[0] > 0:
-            existing = self.df.loc[(self.df["analysis_type"] == analysis_type) & (
-                self.df["options"] == opts)]
+        if isinstance(options, list):
+            # if analysis_type is None:
+            #     raise AttributeError(
+            #         "Analysis type should be provided if options is a list")
+            opts = self.format_options_list(options)
+            new = pd.DataFrame(
+                {"analysis_type": [analysis_type] * len(opts), "options": opts})
+        elif isinstance(options, dict):
+            # if analysis_type is None:
+            #     raise AttributeError(
+            #         "Analysis type should be provided if options is a dict")
+            opts = self.format_options(options)
+            new = pd.DataFrame(
+                [{"analysis_type": analysis_type, "options": opts}])
+        # elif isinstance(options, pd.DataFrame):
+        #     pass
+        else:
+            raise AttributeError(
+                "options attribute should be a list of dicts or a dict")
+
+        # existing = None
+        # opt_id = 0
+
+        # if opts and self.df.shape[0] > 0:
+        #     existing = self.df.loc[(self.df["analysis_type"] == analysis_type) &
+        #                            self.df["options"].isin(opts)]
             # print("existing: " + str(existing))
 
-        if existing is not None and existing.shape[0] > 0:
-            opt_id = int(existing.id.iloc[0])
-        else:
-            opt_id = self.next_id
-            new = pd.DataFrame(
-                [{"id": self.next_id, "analysis_type": analysis_type, "options": str(opts)}])
-            self.next_id += 1
-            dest = self.df
-            self.update(table=dest.append(new, ignore_index=True, sort=True),
-                        save=save)
-        return opt_id
+        # if existing is not None and existing.shape[0] > 0:
+        #     opt_id = int(existing.id.iloc[0])
+        # else:
+
+        res = super().add(new, save, replace)
+        # dest = self.df
+        # self.update(table=dest.append(new, ignore_index=True, sort=True),
+        #             save=save)
+        return res
+
+    def format_options_list(self, option_list):
+        return [self.format_options(options) for options in option_list]
 
     def format_options(self, options):
-        opts = "; ".join([k + ":" + str(v) for k, v in options.items()])
+        opts = ";".join([k + ":" + str(v) for k, v in options.items()])
         return opts
 
-        # new = self.check_ids(new)
-        # if replace:
-        #     # if we replace, remove duplicates from old dataframe
-        #     dest = self.remove_duplicates(self.df, new)
-        # else:
-        #     # remove duplicates from new dataframe
-        #     dest = self.df
-        #     new = self.remove_duplicates(new, self.df)
-        # if not new.empty:
-        #     self.update(table=dest.append(new, ignore_index=True, sort=True),
-        #                 save=save)
+    def expand_options(self, dataframe):
+        # options are stored in the form "option1:value1;option2:value2"
+        # split options to create new dataframe on special characters
+        # : or ;
+        # This creates a new dataframe where each even column is a column name
+        # and every odd column is the associated value
+        tmp = dataframe.options.str.split(r";|:", expand=True)
+        # get number of columns
+        ncols = tmp.shape[1]
+        # get first row
+        row = tmp.iloc[0]
+        # every even colum is actually a column name
+        colnames_idx = list(range(0, ncols, 2))
+        # Save column names
+        colnames = list(row[colnames_idx])
+        colnames = [x.strip() for x in colnames]
+        # Drop useless columns
+        tmp.drop(tmp.columns[colnames_idx], inplace=True, axis=1)
+        # Rename columns
+        tmp.columns = colnames
+        # Concatenate the new dataframe to the old one
+        res = pd.concat([dataframe, tmp], axis=1)
+        return res
