7aeb9d7a3ef3b1cd38fd56e4b818d37d05f88fdf
Vin985 <sylvain.christin@gmail.com>
2020-05-09 17:38:25 -0300

    cleanup + various bug fixes

--- src/analysis/detection/predictions_utils.py
+++ src/analysis/detection/predictions_utils.py
@@ -66,7 +66,6 @@ def detect_events_standard(predictions, recording_id=-1, detection_options=None)
 
 
 def resample_max(x, threshold=0.98, mean_thresh=0):
-    print(x)
     if any(x >= threshold) and x.mean() > mean_thresh:
         return 2
     return 0
@@ -82,7 +81,7 @@ def isolate_events_subsampling(predictions, step):
     tmp = predictions.loc[predictions.event > 0]
     tmp.reset_index(inplace=True)
 
-    step = datetime.timedelta(step)
+    step = datetime.timedelta(milliseconds=step)
     start = None
     events = []
     event_id = 1
@@ -96,13 +95,14 @@ def isolate_events_subsampling(predictions, step):
             if diff > step:
                 end = prev_time + step
                 events.append({"event_id": event_id, "recording_id": x.recording_id,
-                               "start": start, "end": end})
+                               "start": start.timestamp(), "end": end.timestamp()})
                 event_id += 1
                 start = x.datetime
             prev_time = x.datetime
 
+        end = prev_time + step
         events.append({"event_id": event_id, "recording_id": x.recording_id,
-                       "start": start, "end": prev_time+step})
+                       "start": start.timestamp(), "end": end.timestamp()})
 
     events = pd.DataFrame(events)
     return events
@@ -111,20 +111,20 @@ def isolate_events_subsampling(predictions, step):
 def detect_events_subsampling(predictions, recording_id=-1, detection_options=None):
     preds = predictions.copy()
     if not "tag" in preds.columns:
-        preds.loc[:, "tag"] = 0
-    preds.loc[:, "event"] = 0
+        preds.loc[:, "tag"] = -1
+    preds.loc[:, "event"] = -1
     preds.loc[:, "datetime"] = pd.to_datetime(preds.time * 10**9)
     preds.set_index("datetime", inplace=True)
 
     min_activity = detection_options.get("min_activity", 0.85)
     step = detection_options.get("min_duration", 0.1) * 1000
     isolate_events = detection_options.get("isolate_events", False)
-    print(step)
 
     resampled = preds.resample(str(step)+"ms")
     resample_func = functools.partial(resample_max, threshold=min_activity)
     res = resampled.agg({"activity": resample_func,
                          "tag": has_tag})
+    res.rename(columns={"activity": "event"}, inplace=True)
     res["recording_id"] = recording_id
 
     if isolate_events:
