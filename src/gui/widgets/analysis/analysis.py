df9356e5934bda1d16ed7dee6f16886cad2d8058
Vin985 <sylvain.christin@gmail.com>
2020-06-05 17:13:08 -0400

    page widget

--- src/gui/widgets/analysis/analysis.py
+++ src/gui/widgets/analysis/analysis.py
@@ -1,13 +1,17 @@
-from PySide2.QtCore import Slot
-from PySide2.QtWidgets import QApplication, QWidget
+from gui.widgets.common.page_widget import PageWidget
 
 from gui.widgets.analysis.ui.analysis_ui import Ui_Analysis
+import resource
 
 
-class Analysis(QWidget, Ui_Analysis):
+class Analysis(PageWidget, Ui_Analysis):
     def __init__(self):
+        print('analysis1: Memory usage: %s (kb)' %
+              resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
         super().__init__()
         self.setupUi(self)
+        print('analysis2: Memory usage: %s (kb)' %
+              resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
         self.link_events()
 
     # Define callbacks when events happen
