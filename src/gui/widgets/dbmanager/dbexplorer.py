a346be01ce7411f5b71c6aa1ac9a41ffd7c3a610
Vin985 <sylvain.christin@gmail.com>
2020-06-05 17:09:37 -0400

    changed table display logic removed import file changed export table

--- src/gui/widgets/dbmanager/dbexplorer.py
+++ src/gui/widgets/dbmanager/dbexplorer.py
@@ -1,53 +1,64 @@
-from PySide2.QtGui import qApp
-from PySide2.QtWidgets import QWidget
+
+#import resource
 
 from gui.utils.dataframeTableModel import DataFrameTableModel
-from gui.widgets.dbmanager.fileimport import FileImport
 from gui.widgets.dbmanager.ui.dbexplorer_ui import Ui_DBExplorer
+from gui.widgets.common.page_widget import PageWidget
 
 
-class DBExplorer(QWidget, Ui_DBExplorer):
-    def __init__(self, recordings=None):
+class DBExplorer(PageWidget, Ui_DBExplorer):
+    def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.current_table = None
-        recordings = qApp.get_recordings()
-
-        self.rowsFound.setText(
-            "{0} recording(s) found!".format(len(recordings)))
+        self.action_dialog = None
         self.linkEvents()
         self.addAction(self.action_ACI)
         self.init_table_selector()
-        self.initTableView()
+
+    # def enter_page(self):
+    #     print(self.comboBox_table.currentIndex())
+    #     self.display_table(self.comboBox_table.currentIndex())
+    #     print('dbexplorer4: Memory usage: %s (kb)' %
+    #           resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
+
+    # def leave_page(self):
+    #     self.setTableModel(None)
 
     def linkEvents(self):
-        self.dbImportButton.clicked.connect(self.showImportWindow)
-        self.comboBox_table.currentIndexChanged.connect(self.display_table)
+        self.combo_table.currentIndexChanged.connect(self.display_table)
         self.btn_export.clicked.connect(self.export_table)
 
     def init_table_selector(self):
         tables = qApp.tables.list_tables()
         for table in tables:
             display_name = table.replace("_", " ")
-            self.comboBox_table.addItem(display_name, table)
-
-    def initTableView(self):
-        if qApp.get_recordings().empty:
-            self.dbTable.setEnabled(False)
-        else:
-            # model["date"] = model["date"].dt.strftime("%Y-%m-%d %H:%M:%S")
-            # self.setTableModel(qApp.get_recordings())
-            self.dbTable.resizeColumnsToContents()
+            self.combo_table.addItem(display_name, table)
+        self.display_table(self.combo_table.currentIndex())
 
     def export_table(self):
-        self.current_table.to_feather(
-            self.comboBox_table.currentText() + ".feather")
+        # TODO use dbmanager instead
+        index = self.combo_table.currentIndex()
+        if index:
+            table = self.combo_table.itemData(index)
+            from gui.widgets.dialogs.export_table_dialog import ExportTableDialog
+            self.action_dialog = ExportTableDialog(table, parent=self)
+            self.action_dialog.setModal(True)
+            self.action_dialog.show()
 
     def display_table(self, index):
-        table = self.comboBox_table.itemData(index)
-        model = qApp.tables.get_table(table).df
+        model = []
+        text = ""
+        if index:
+            if not self.btn_export.isEnabled():
+                self.btn_export.setEnabled(True)
+            table = self.combo_table.itemData(index)
+            model = qApp.tables.get_table(table).df
+            text = "{0} entries(s) found!".format(len(model))
+        else:
+            self.btn_export.setEnabled(False)
+
         self.setTableModel(model)
-        self.current_table = model
+        self.rowsFound.setText(text)
 
     def setTableModel(self, data):
         model = DataFrameTableModel(data)
@@ -57,10 +68,4 @@ class DBExplorer(QWidget, Ui_DBExplorer):
         # self.dbTable.setModel(proxyModel)
 
     def refresh_table(self):
-        self.setTableModel(qApp.get_recordings())
-        print("aaaah refreshing!!!")
-
-    def showImportWindow(self):
-        self.file_import = FileImport()
-        self.file_import.finished.connect(self.refresh_table)
-        self.file_import.show()
+        pass
