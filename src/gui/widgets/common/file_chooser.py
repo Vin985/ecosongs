d8f5741c85f46aeefe4523fa00fd49ff51fa6b3f
Vin985 <sylvain.christin@gmail.com>
2020-06-11 00:23:44 -0400

    file chooser widget

---
+++ src/gui/widgets/common/file_chooser.py
@@ -0,0 +1,62 @@
+
+import os
+
+from PySide2.QtWidgets import QWidget, QFileDialog
+
+from gui.widgets.common.ui.file_chooser_ui import Ui_FileChooser
+# from ui.file_chooser_ui import Ui_FileChooser
+
+
+class FileChooser(QWidget, Ui_FileChooser):
+
+    def __init__(self, options=None, parent=None):
+        super().__init__(parent=parent)
+        self.options = options or {}
+        self.setupUi(self)
+        self.link_events()
+
+    def link_events(self):
+        self.btn_browse.clicked.connect(self.browse)
+
+    def init_display(self):
+        self.input_path.setText(self.options.get("default", ""))
+
+    def browse(self):
+        file_type = self.options["file_type"]
+        if not file_type:
+            raise AttributeError("No type of dialog type defined. Please add the 'file_type' option"
+                                 "to the FileChooser class.")
+        text = getattr(self, "choose_" + file_type)()
+        # Update gui input
+        if text:
+            self.input_path.setText(text)
+
+    def choose_folder(self):
+        default = self.input_path.text() or os.getcwd()
+        text = QFileDialog.getExistingDirectory(self, "Choose directory",
+                                                default)
+        return text
+
+    def choose_files(self):
+        default = self.input_path.text() or os.getcwd()
+        file_filter = self.options.get("file_filter", "")
+        (files, _) = QFileDialog.getOpenFileNames(
+            self, "Choose files", default, file_filter)
+        text = "; ".join(files)
+        return text
+
+    def choose_save_file(self):
+        default = self.input_path.text() or os.getcwd()
+        file_filter = self.options.get("file_filter", "")
+        (text, _) = QFileDialog.getSaveFileName(
+            self, "Choose file", default, file_filter)
+        return text
+
+
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     options = {"file_type": "save_file",
+#                "default": "/mnt/win/UMoncton/Doctorat/dev/ecosongs/src/gui/widgets/common/file_chooser.py"}
+#     fc = FileChooser(options=options)
+#     fc.show()
+#     app.exec_()
---
+++ src/gui/widgets/common/ui/file_chooser_ui.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'file_chooser.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import (QCoreApplication, QDate, QDateTime, QMetaObject,
+    QObject, QPoint, QRect, QSize, QTime, QUrl, Qt)
+from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
+    QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter,
+    QPixmap, QRadialGradient)
+from PySide2.QtWidgets import *
+
+
+class Ui_FileChooser(object):
+    def setupUi(self, FileChooser):
+        if not FileChooser.objectName():
+            FileChooser.setObjectName(u"FileChooser")
+        FileChooser.resize(492, 59)
+        self.horizontalLayout = QHBoxLayout(FileChooser)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.input_path = QLineEdit(FileChooser)
+        self.input_path.setObjectName(u"input_path")
+
+        self.horizontalLayout.addWidget(self.input_path)
+
+        self.btn_browse = QPushButton(FileChooser)
+        self.btn_browse.setObjectName(u"btn_browse")
+
+        self.horizontalLayout.addWidget(self.btn_browse)
+
+
+        self.retranslateUi(FileChooser)
+
+        QMetaObject.connectSlotsByName(FileChooser)
+    # setupUi
+
+    def retranslateUi(self, FileChooser):
+        FileChooser.setWindowTitle(QCoreApplication.translate("FileChooser", u"Form", None))
+        self.btn_browse.setText(QCoreApplication.translate("FileChooser", u"Browse...", None))
+    # retranslateUi
+
