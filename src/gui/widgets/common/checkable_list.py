03cc246d23d64870e8c9573aa196a102ae998075
Vin985 <sylvain.christin@gmail.com>
2020-06-12 17:02:55 -0400

    convenience function for chackable list

--- src/gui/widgets/common/checkable_list.py
+++ src/gui/widgets/common/checkable_list.py
@@ -1,13 +1,15 @@
-import sys
+# import sys
 
-from PySide2.QtWidgets import QApplication
+# from PySide2.QtWidgets import QApplication
 
 from PySide2.QtWidgets import QListWidget, QListWidgetItem
-from PySide2.QtCore import Qt
-from PySide2.QtGui import QColor, QPalette
+from PySide2.QtCore import Qt, Signal
+from PySide2.QtGui import QPalette
 
 
 class CheckableList(QListWidget):
+
+    selection_changed = Signal()
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -18,7 +20,6 @@ class CheckableList(QListWidget):
 
     def linkEvents(self):
         self.itemDoubleClicked.connect(self.select_item)
-        # self.itemPressed.connect(self.select_item)
         self.itemChanged.connect(self.update_selected)
 
     def update_selected(self, item):
@@ -27,7 +28,7 @@ class CheckableList(QListWidget):
         else:
             if item.text() in self.selected:
                 self.selected.remove(item.text())
-        print(self.selected)
+        self.selection_changed.emit()
 
     def select_item(self, item):
         self.revert_item_state(item)
@@ -45,11 +46,23 @@ class CheckableList(QListWidget):
             state = Qt.Checked if checked else Qt.Unchecked
             item.setCheckState(state)
 
+    def select_all(self):
+        for i in range(0, self.count()):
+            item = self.item(i)
+            item.setCheckState(Qt.Checked)
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    test_list = ["toto", "tutu", "hello", "world"]
-    cl = CheckableList()
-    cl.addItems(test_list, True)
-    cl.show()
-    app.exec_()
+    def deselect_all(self):
+        for i in range(0, self.count()):
+            item = self.item(i)
+            item.setCheckState(Qt.Unchecked)
+
+
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     test_list = ["toto", "tutu", "hello", "world"]
+#     cl = CheckableList()
+#     cl.addItems(test_list)
+#     cl.select_all()
+#     cl.deselect_all()
+#     cl.show()
+#     app.exec_()
