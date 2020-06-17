a18faebddcd9aabeea22ab47c008d8ebfe761bb4
Vin985 <sylvain.christin@gmail.com>
2020-06-17 16:55:48 -0400

    table loading + column check

--- src/db/models.py
+++ src/db/models.py
@@ -50,20 +50,6 @@ class TableModel():
     def __str__(self):
         return str(self.df)
 
-    def load_data(self):
-        try:
-            if not self.dbmanager:
-                raise AttributeError(
-                    "No database manager provided. Cannot load data")
-            if not self.TABLE_NAME:
-                raise AttributeError(
-                    "No table name provided. Cannot load data")
-            self.df = self.dbmanager.get_table(self.TABLE_NAME)
-            return(self._df.loc[:, self._df.columns.intersection(self.columns)])
-        except Exception as e:
-            print(e)
-            self._df = pd.DataFrame()
-
     @property
     def df(self):
         if self._df is None:
@@ -72,16 +58,25 @@ class TableModel():
 
     @df.setter
     def df(self, df):
-        self._df = self.check_ids(df)
+        self._df = self.check_columns(df)
 
     @property
     def empty(self):
         return self.df.empty
 
-    def check_types(self):
-        if self.COLUMNS_TYPE:
-            for k, v in self.COLUMNS_TYPE.items():
-                self._df[k] = self._df[k].astype(v)
+    def load_data(self):
+        self.df = self.get_table(table=self.TABLE_NAME)
+
+    def get_table(self, **kwargs):
+        df = pd.DataFrame()
+        if not self.dbmanager:
+            raise AttributeError(
+                "No database manager provided. Cannot load data")
+        if not self.TABLE_NAME:
+            raise AttributeError(
+                "No table name provided. Cannot load data")
+        df = self.dbmanager.get_table(**kwargs)
+        return df
 
     def save(self, update=False):
         self.check_types()
@@ -100,7 +95,7 @@ class TableModel():
         if save:
             self.save()
 
-    def update_table(self, table, **kwargs):
+    def update_table(self, table):
         self.df = table
 
     def query(self, query):
@@ -116,6 +111,24 @@ class TableModel():
         if not table.empty:
             self.next_id = max(table["id"]) + 1
         return table
+
+    def check_types(self, df=None):
+        if self.COLUMNS_TYPE:
+            if df is None:
+                df = self._df
+            for k, v in self.COLUMNS_TYPE.items():
+                # TODO: do only if type is different
+                df[k] = df[k].astype(v)
+
+    def check_columns(self, df):
+        df = self.check_ids(df)
+        if not all(item in df.columns for item in self.columns):
+            raise ValueError(
+                "Not all required columns are present in the loaded table. Please make sure the" +
+                " loaded table is the correct one. Required columns are {0}. Only found {1}."
+                .format(self.columns, df.columns))
+        self.check_types(df)
+        return df
 
     def get_duplicates_dict(self, df):
         return df[self.DUPLICATE_COLUMNS].to_dict(orient='list')
@@ -167,6 +180,3 @@ class TableModel():
     def export_table(self, options, table=None):
         to_export = table if table is not None else self.df
         self.dbmanager.export_table(to_export, options["dest"])
-
-    def import_table(self):
-        pass
