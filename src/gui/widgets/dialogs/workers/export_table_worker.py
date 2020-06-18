31cc814a44bb1fb31185443a3ccc318559f94d95
Vin985 <sylvain.christin@gmail.com>
2020-06-17 20:39:42 -0400

    add table prefix when exporting with expanded columns

--- src/gui/widgets/dialogs/workers/export_table_worker.py
+++ src/gui/widgets/dialogs/workers/export_table_worker.py
@@ -16,11 +16,26 @@ class ExportTableWorker(ThreadWorker):
         table_model = qApp.tables.get_table(self.table_name)
         to_export = self.table_data.copy()
 
+        # if self.table_name == "activity_predictions":
+        #     to_export = to_export.iloc[1:50000]
+
         for expand_table, expand_columns in self.options["expand"].items():
             expand_data = qApp.tables.get_table(expand_table).df
             merge_on = table_model.REFERS_TO[expand_table]["on"].split(":")
+
+            # rename columns to prefix name of table
+            new_cols = {col: expand_table + "_" +
+                        col for col in expand_columns}
+
+            # Add the column to merge on
             expand_columns.add(merge_on[1])
-            expand_data = expand_data[expand_columns]
+
+            # Only keep relevant columns
+            expand_data = expand_data[expand_columns].copy()
+
+            # Rename columns
+            expand_data.rename(columns=new_cols, inplace=True)
+
             to_export = to_export.merge(
                 expand_data, left_on=merge_on[0], right_on=merge_on[1])
             to_export.drop(columns=merge_on, inplace=True)
