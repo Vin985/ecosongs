7b70ddf09181f0635ebbd3f3864782a81e2b4400
Vin985 <sylvain.christin@gmail.com>
2020-06-23 14:27:49 -0400

    default database path

--- src/ecosongs.py
+++ src/ecosongs.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+import resource
 
 from PySide2.QtWidgets import QApplication
 
@@ -8,6 +9,8 @@ from db.tablemanager import TableManager
 from gui.ecosongsUI import EcosongsUI
 from gui.utils.settings import Settings
 
+# import tracemalloc
+
 # # print(os.environ['LD_LIBRARY_PATH'])
 #
 # print(sys.version)
@@ -15,7 +18,6 @@ from gui.utils.settings import Settings
 
 
 class Ecosongs(QApplication):
-
     def get_recordings(self, df=True):
         if df:
             return self.tables.recordings.df
@@ -34,18 +36,40 @@ class Ecosongs(QApplication):
         db_opts = settings.group_to_dict("database")
         if not db_opts:
             print("adding database defaults")
+            # TODO: Change defaults!
             db_opts.update(
-                {"database": "ecosongs", "db_type": "feather", "path": "/mnt/win/UMoncton/Doctorat/dev/ecosongs/src/db"})
+                {"database": "ecosongs", "db_type": "feather", "path": "db",}
+            )
 
         self.dbmanager = dbutils.get_db_manager(**db_opts)
         self.tables = TableManager(self.dbmanager)
+        print(
+            "Ecosongs init: Memory usage: %s (kb)"
+            % resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+        )
 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
+    # tracemalloc.start(25)
     # logging.basicConfig(filename='example.log', level=logging.DEBUG)
     logging.basicConfig(level=logging.DEBUG)
+    print(
+        "Main: Memory usage: %s (kb)"
+        % resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+    )
     app = Ecosongs(sys.argv)
+    print(
+        "Main2: Memory usage: %s (kb)"
+        % resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+    )
     ui = EcosongsUI()
+    # snapshot = tracemalloc.take_snapshot()
+    # top_stats = snapshot.statistics('traceback')
+
+    # # pick the biggest memory block
+    # stat = top_stats[0]
+    # print("%s memory blocks: %.1f KiB" % (stat.count, stat.size / 1024))
+    # for line in stat.traceback.format():
+    #     print(line)
     ui.showMaximized()
     sys.exit(app.exec_())
