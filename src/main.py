619f13ff14a723ed74fc7cbf93072afc8c5daaf2
Vin985 <sylvain.christin@gmail.com>
2020-06-25 14:46:28 -0400

    changed main.py

--- src/main.py
+++ src/main.py
@@ -1,14 +1,16 @@
-from PySide2.QtWidgets import QApplication, QMainWindow
-
-if __name__ == '__main__':
+if __name__ == "__main__":
+    print("using main")
     import sys
-    app = QApplication(sys.argv)
+    from ecosongs import Ecosongs
+    from gui.ecosongsUI import EcosongsUI
+
+    app = Ecosongs(sys.argv)
 
     # f = QFile("gui/resources/qss/custom.qss")
     # f.open(QFile.ReadOnly)
     # res = QTextStream(f).readAll()
     # print(res)
     # app.setStyleSheet(res)
-    ui = Ecosongs()  # We set the form to be our ExampleApp (design)
+    ui = EcosongsUI()  # We set the form to be our ExampleApp (design)
     ui.showMaximized()
     sys.exit(app.exec_())
