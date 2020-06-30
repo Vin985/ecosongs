ef2f0c3d7bc6330c4da1bb93be0f1ebf4cb88fe8
Vin985 <sylvain.christin@gmail.com>
2020-06-30 12:41:52 -0400

    formatting

--- src/analysis/detection/lib/tf_classifier.py
+++ src/analysis/detection/lib/tf_classifier.py
@@ -12,7 +12,7 @@ from analysis.detection.lib import train_helpers
 from analysis.spectrogram import Spectrogram
 
 # Custom functions and classes
-sys.path.append('../lib')
+sys.path.append("../lib")
 
 N_FFT = 2048
 HOP_LENGTH = 1024  # 512
@@ -20,16 +20,12 @@ N_MELS = 32  # 128
 
 
 class TFClassifier(object):
-
     def __init__(self, opts, weights_path):
         """Create the layers of the neural network, with the same options we used in training"""
         self.opts = namedtuple("opts", opts.keys())(*opts.values())
         tf.reset_default_graph()
 
-        # config = tf.ConfigProto(
-        #     # device_count={'GPU': 0},
-        #     #log_device_placement=True
-        # )
+        # config = tf.ConfigProto(device_count={"GPU": 0}, log_device_placement=True)
         # self.sess = tf.Session(config=config)
 
         self.sess = tf.Session()
@@ -45,24 +41,30 @@ class TFClassifier(object):
         # Create an object which will iterate over the test spectrograms
         # appropriately
         self.test_sampler = train_helpers.SpecSampler(
-            batch_size=256, hww_x=self.opts.HWW_X, hww_y=self.opts.HWW_Y,
-            do_aug=False, learn_log=self.opts.LEARN_LOG,
-            randomise=False, seed=10, balanced=False)
+            batch_size=256,
+            hww_x=self.opts.HWW_X,
+            hww_y=self.opts.HWW_Y,
+            do_aug=False,
+            learn_log=self.opts.LEARN_LOG,
+            randomise=False,
+            seed=10,
+            balanced=False,
+        )
 
     def __exit__(self):
         # tear down the tensorflow session
         self.sess.close()
 
-    def load_wav(self, wavpath, loadmethod='librosa'):
+    def load_wav(self, wavpath, loadmethod="librosa"):
         # tic = time()
 
-        if loadmethod == 'librosa':
+        if loadmethod == "librosa":
             # a more correct and robust way -
             # this resamples any audio file to 22050Hz
             # TODO: downsample if higher than 22050
             sample_rate = 22050 if self.opts.resample else None
             self.wav, self.sample_rate = librosa.load(wavpath, sr=sample_rate)
-        elif loadmethod == 'wavfile':
+        elif loadmethod == "wavfile":
             # a hack for speed - resampling is done assuming raw audio is
             # sampled at 44100Hz. Not recommended for general use.
             self.sample_rate, self.wav = wavfile.read(open(wavpath))
@@ -73,8 +75,13 @@ class TFClassifier(object):
 
     def compute_spec(self):
         # tic = time()
-        spec = melspectrogram(self.wav, sr=self.sample_rate, n_fft=N_FFT,
-                              hop_length=HOP_LENGTH, n_mels=N_MELS)
+        spec = melspectrogram(
+            self.wav,
+            sr=self.sample_rate,
+            n_fft=N_FFT,
+            hop_length=HOP_LENGTH,
+            n_mels=N_MELS,
+        )
 
         if self.opts.remove_noise:
             spec = Spectrogram.remove_noise(spec)
@@ -87,18 +94,16 @@ class TFClassifier(object):
         """Apply the classifier"""
         tic = time()
         if wavpath is not None:
-            self.load_wav(wavpath, loadmethod='librosa')
+            self.load_wav(wavpath, loadmethod="librosa")
             self.compute_spec()
 
         labels = np.zeros(self.spec.shape[1])
-        print("Took %0.3fs to load" % (time() - tic))
+        # print("Took %0.3fs to load" % (time() - tic))
         tic = time()
         probas = []
         for Xb, _ in self.test_sampler([self.spec], [labels]):
-            pred = self.sess.run(
-                self.net['output'],
-                feed_dict={self.net['input']: Xb})
+            pred = self.sess.run(self.net["output"], feed_dict={self.net["input"]: Xb})
             probas.append(pred)
-        print("Took %0.3fs to classify" % (time() - tic))
+        # print("Took %0.3fs to classify" % (time() - tic))
 
         return np.vstack(probas)[:, 1]
