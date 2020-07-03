a8bb47b0dd9ee8ffcc779cd1e0f70cac48ae18e1
Vin985 <sylvain.christin@gmail.com>
2020-07-02 21:25:01 -0400

    dbmanager name + formatting

--- src/db/dbmanager.py
+++ src/db/dbmanager.py
@@ -2,30 +2,26 @@ from abc import ABC, abstractmethod
 
 
 class DBManager(ABC):
-    SAVE_EXTENSION = ""
+    NAME = ""
+    FILE_EXTENSION = ""
 
     def __init__(self):
         pass
 
     def save(self, table, data):
-        raise NotImplementedError(
-            "Function not implemented for this DB manager")
+        raise NotImplementedError("Function not implemented for this DB manager")
 
     def update(self, table, data):
-        raise NotImplementedError(
-            "Function not implemented for this DB manager")
+        raise NotImplementedError("Function not implemented for this DB manager")
 
     def delete(self, table, data):
-        raise NotImplementedError(
-            "Function not implemented for this DB manager")
+        raise NotImplementedError("Function not implemented for this DB manager")
 
     def export_table(self, data, dest_path):
-        raise NotImplementedError(
-            "Function not implemented for this DB manager")
+        raise NotImplementedError("Function not implemented for this DB manager")
 
     def import_table(self, src_path):
-        raise NotImplementedError(
-            "Function not implemented for this DB manager")
+        raise NotImplementedError("Function not implemented for this DB manager")
 
     @abstractmethod
     def get_table(self, table=None, path=None):
