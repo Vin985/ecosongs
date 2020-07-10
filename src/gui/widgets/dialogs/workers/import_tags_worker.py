61ad7dccb5b7b477b1261e65453a9fdb334c0e6a
Vin985 <sylvain.christin@gmail.com>
2020-07-09 21:20:31 -0400

    bug fix to update has_tags column on tag import

--- src/gui/widgets/dialogs/workers/import_tags_worker.py
+++ src/gui/widgets/dialogs/workers/import_tags_worker.py
@@ -7,16 +7,20 @@ from gui.threads.thread_worker import ThreadWorker
 class ImportTagsWorker(ThreadWorker):
     def __init__(self, recordings):
         super().__init__()
-        self.recordings = recordings
+        self.recordings = recordings[["id", "name", "path", "has_tags"]].copy()
 
     def perform_task(self):
         print("Importing tags")
         self.options["save"] = True
         self.results = tag_manager.load_tags(
-            self.recordings, self.options["task_options"]
+            self.recordings, self.options["task_options"],
         )
 
     def save_results(self):
         tags_table = qApp.tables.tags
-        tags_table.add(self.results, save=True, replace=True)
+        tags, recs = self.results
+        tags_table.add(tags, save=True, replace=True)
+        recs_df = qApp.tables.recordings.df
+        recs_df.update(recs)
         qApp.tables.recordings.save()
+
