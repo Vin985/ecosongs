f19ab4fb9142485c04e3579c1b580db903b3d06c
Vin985 <sylvain.christin@gmail.com>
2020-09-04 14:59:09 -0400

    do not save intermediate spectrograms

--- src/analysis/detection/lib/tf_classifier.py
+++ src/analysis/detection/lib/tf_classifier.py
@@ -18,6 +18,27 @@ N_MELS = 32  # 128
 
 
 class CityNetClassifier1(object):
+    def count_number_trainable_params(self):
+        """
+        Counts the number of trainable variables.
+        """
+        tot_nb_params = 0
+        for trainable_variable in tf.compat.v1.trainable_variables():
+            shape = trainable_variable.get_shape()  # e.g [D,F] or [W,H,C]
+            current_nb_params = self.get_nb_params_shape(shape)
+            tot_nb_params = tot_nb_params + current_nb_params
+        return tot_nb_params
+
+    def get_nb_params_shape(self, shape):
+        """
+        Computes the total number of params for a given shap.
+        Works for any number of shapes etc [D,F] or [W,H,C] computes D*F and W*H*C.
+        """
+        nb_params = 1
+        for dim in shape:
+            nb_params = nb_params * int(dim)
+        return nb_params
+
     def __init__(self, opts, weights_path):
         """Create the layers of the neural network, with the same options we used in training"""
         self.opts = namedtuple("opts", opts.keys())(*opts.values())
@@ -40,6 +61,10 @@ class CityNetClassifier1(object):
         train_saver.restore(self.sess, weights_path)
 
         print("loaded")
+
+        print(
+            "Number of trainable parameters: %d" % self.count_number_trainable_params()
+        )
 
         # Create an object which will iterate over the test spectrograms
         # appropriately
@@ -99,24 +124,21 @@ class CityNetClassifier1(object):
             # this resamples any audio file to 22050Hz
             # TODO: downsample if higher than 22050
             sample_rate = 22050 if self.opts.resample else None
-            self.wav, self.sample_rate = librosa.load(wavpath, sr=sample_rate)
+            return librosa.load(wavpath, sr=sample_rate)
         elif loadmethod == "wavfile":
             # a hack for speed - resampling is done assuming raw audio is
             # sampled at 44100Hz. Not recommended for general use.
-            self.sample_rate, self.wav = wavfile.read(open(wavpath))
-            self.wav = self.wav[::2] / 32768.0
-            self.sample_rate /= 2
+            sample_rate, wav = wavfile.read(open(wavpath))
+            wav = wav[::2] / 32768.0
+            sample_rate /= 2
+            return (wav, sample_rate)
         else:
             raise Exception("Unknown load method")
 
-    def compute_spec(self):
+    def compute_spec(self, wav, sample_rate):
         # tic = time()
         spec = melspectrogram(
-            self.wav,
-            sr=self.sample_rate,
-            n_fft=N_FFT,
-            hop_length=HOP_LENGTH,
-            n_mels=N_MELS,
+            wav, sr=sample_rate, n_fft=N_FFT, hop_length=HOP_LENGTH, n_mels=N_MELS,
         )
 
         if self.opts.remove_noise:
@@ -124,24 +146,24 @@ class CityNetClassifier1(object):
 
         spec = np.log(self.opts.A + self.opts.B * spec)
         spec = spec - np.median(spec, axis=1, keepdims=True)
-        self.spec = spec.astype(np.float32)
+        return spec.astype(np.float32)
 
     def classify(self, wavpath=None):
         """Apply the classifier"""
         tic = time()
-        
-        if wavpath is not None:
-            self.load_wav(wavpath, loadmethod="librosa")
-            self.compute_spec()
 
-        labels = np.zeros(self.spec.shape[1])
+        if wavpath is not None:
+            wav, sr = self.load_wav(wavpath, loadmethod="librosa")
+            spec = self.compute_spec(wav, sr)
+
+        labels = np.zeros(spec.shape[1])
         # print("Took %0.3fs to load" % (time() - tic))
         tic = time()
         probas = []
-        for Xb, _ in self.test_sampler([self.spec], [labels]):
+        for Xb, _ in self.test_sampler([spec], [labels]):
             pred = self.sess.run(self.net["output"], feed_dict={self.net["input"]: Xb})
             probas.append(pred)
         # print("Took %0.3fs to classify" % (time() - tic))
-        print("Classified {0} in {1}".format(wavpath, time()-tic))
+        print("Classified {0} in {1}".format(wavpath, time() - tic))
 
-        return np.vstack(probas)[:, 1]
+        return (np.vstack(probas)[:, 1], sr)
